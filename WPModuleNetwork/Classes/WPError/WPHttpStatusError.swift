7998b6a80122a017c26a6d8f453002a5696f22a5
AhnSangHoon <ansh0324@gmail.com>
2021-05-13 18:15:01 +0900

    FIX ] WPHttpStatusError

--- WPModuleNetwork/Classes/WPError/WPHttpStatusError.swift
+++ WPModuleNetwork/Classes/WPError/WPHttpStatusError.swift
@@ -16,24 +16,29 @@ public struct WPHttpStatusError {
         self.code = code
         self.status = status
     }
+    
+    var errorMessage: String {
+        return "\(code): " + status.description
+    }
 }
 
 /// Http Status Code에 따른 Status Case
 public enum WPHttpStatus {
-    /// # 100 - 199 사이의 100 번대 Status
+    /// 100 - 199 사이의 100 번대 Status
     case conditionalResponse
-    /// # 200 - 299 사이의 200 번대 Status
+    /// 200 - 299 사이의 200 번대 Status
     case success
-    /// # 300 - 399 사이의 300 번대 Status
+    /// 300 - 399 사이의 300 번대 Status
     case redirection
-    /// # 400 - 499 사이의 400 번대 Status
-    case clientRequestError
-    /// # 500 - 599 사이의 500 번대 Status
+    /// 400 - 499 사이의 400 번대 Status
+    case badRequestError
+    /// 500 - 599 사이의 500 번대 Status
     case serverSideError
-    /// # 명시되지 않은 Status
+    /// 명시되지 않은 Status
     case other
-    
-    var message: String {
+
+    /// 각 case에 대한 description
+    var description: String {
         switch self {
         case .conditionalResponse:
             return "ConditionalResponse"
@@ -41,15 +46,15 @@ public enum WPHttpStatus {
             return "Success"
         case .redirection:
             return "Redirection"
-        case .clientRequestError:
-            return "ClientRequestError"
+        case .badRequestError:
+            return "BadRequestError"
         case .serverSideError:
             return "ServerSideError"
         case .other:
             return "OtherHttpStatusError"
         }
     }
-    
+
     static func get(from code: Int) -> WPHttpStatus {
         switch code {
         case 100..<200:
@@ -59,7 +64,7 @@ public enum WPHttpStatus {
         case 300..<400:
             return .redirection
         case 400..<500:
-            return .clientRequestError
+            return .badRequestError
         case _ where code > 500:
             return .serverSideError
         default:
